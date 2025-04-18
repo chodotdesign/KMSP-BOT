886861ff90e2ecd8818251d96c0d90bddbf0a3e2
chodotdesign <159584698+chodotdesign@users.noreply.github.com>
2025-04-19 05:59:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  TOKEN: 'BOT_TOKEN',
+  TOKEN: 'bot7675551303:AAGXyTCgYh63_fpQrbWGb6YIa9CPpxAM9bk',
   SELLER_CODE: 'SELLER_KODE_KMSP',
-  OWNER_NAME: 'Sonzai X シ',
-  OWNER_USERNAME: 'November2k',
-  OWNER_ID: 'OWNER_ID',
+  OWNER_NAME: 'ndygaming,
+  OWNER_USERNAME: 'herdiansyahh098',
+  OWNER_ID: '1719183107',
   MONGO_DB: 'MONGO_DB_URL',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
